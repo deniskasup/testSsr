c3bbe9ad81b378b0ad91b514c092ef443f0485b9
Denis Kosorotov <deniskasup@gmail.com>
2022-11-18 14:52:58 +0300

    страница акций

---
+++ src/components/CategorySales.vue
@@ -0,0 +1,112 @@
+<template>
+    <section class="category-sales">
+        <Wrapper class="category-sales__wrapper">
+            <h2 class="category-sales__title">Скидки в категориях</h2>
+            <div class="category-sales__cards category-sales-cards">
+                <NuxtLink
+                    v-for="item in 6"
+                    :key="`category-sales-cards-item-${item}`"
+                    to="/"
+                    class="category-sales-cards__item category-sales-cards-item"
+                >
+                    <div class="category-sales-cards-item__title">Кровати</div>
+                    <div class="category-sales-cards-item__tag">20%</div>
+                    <nuxt-img
+                        loading="lazy"
+                        class="category-sales-cards-item__img"
+                        format="webp"
+                        :src="`assets/images/showcase/showcase-beds.png`"
+                    />
+                </NuxtLink>
+            </div>
+        </Wrapper>
+    </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="sass" scoped>
+.category-sales
+    &__wrapper
+        display: grid
+        grid-gap: 40px
+    &__title
+        font-weight: 700
+        font-size: 26px
+        line-height: 30px
+        +until-tablet
+            font-size: 24px
+            line-height: 24px
+        +while-mob
+            font-size: 20px
+
+.category-sales-cards
+    display: grid
+    grid-template-columns: 1fr 1fr 1fr
+    grid-gap: 30px
+    +media-until(767px)
+        grid-template-columns: 1fr 1fr
+        grid-gap: 20px
+    +while-mob
+        grid-gap: 16px
+
+.category-sales-cards-item
+    position: relative
+    padding-top: 100%
+    +while-mob
+        padding-top: 147%
+    &__img
+        position: absolute
+        top: 0
+        left: 0
+        height: 100%
+        width: 100%
+        transition: transform .3s
+
+        &:hover
+            transform: scale(1.01)
+
+    &__tag
+        position: absolute
+        top: 20px
+        right: 12px
+        z-index: 2
+        width: 48px
+        height: 30px
+        display: flex
+        align-items: center
+        justify-content: center
+        padding-left: 10px
+        font-weight: 700
+        font-size: 14px
+        color: $color_primary
+        background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDkiIGhlaWdodD0iMzAiIHZpZXdCb3g9IjAgMCA0OSAzMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMS4yNzg4IDBDMTAuNTk5NCAwIDkuOTY2NTIgMC4zNDQ4NTEgOS41OTgyMSAwLjkxNTY5NUwxLjIxMDU1IDEzLjkxNTdDMC43ODQ2MzQgMTQuNTc1OCAwLjc4NDYzMyAxNS40MjQyIDEuMjEwNTUgMTYuMDg0M0w5LjU5ODIxIDI5LjA4NDNDOS45NjY1MiAyOS42NTUxIDEwLjU5OTQgMzAgMTEuMjc4OCAzMEg0Ni44OTE4QzQ3Ljk5NjQgMzAgNDguODkxOCAyOS4xMDQ2IDQ4Ljg5MTggMjhWMkM0OC44OTE4IDAuODk1NDMxIDQ3Ljk5NjQgMCA0Ni44OTE4IDBIMTEuMjc4OFpNNy43NzA3IDE3LjVDOS4xMDY2NCAxNy41IDEwLjE4OTYgMTYuMzgwNyAxMC4xODk2IDE1QzEwLjE4OTYgMTMuNjE5MyA5LjEwNjY0IDEyLjUgNy43NzA3IDEyLjVDNi40MzQ3NyAxMi41IDUuMzUxNzggMTMuNjE5MyA1LjM1MTc4IDE1QzUuMzUxNzggMTYuMzgwNyA2LjQzNDc3IDE3LjUgNy43NzA3IDE3LjVaIiBmaWxsPSIjRjJGMkYyIi8+Cjwvc3ZnPgo=)
+        background-size: contain
+        background-repeat: no-repeat
+        +until-tablet
+            top: 15px
+        +while-mob
+            padding-left: 7px
+            width: 42px
+            height: 24px
+            font-size: 12px
+            top: 8px
+            right: 8px
+    &__title
+        z-index: 2
+        position: absolute
+        top: 20px
+        left: 20px
+        font-weight: 600
+        font-size: 20px
+        line-height: 24px
+        +until-tablet
+            line-height: 1.4
+            font-size: 18px
+            top: 15px
+            left: 15px
+        +while-mob
+            top: 8px
+            left: 8px
+            font-size: 14px
+</style>
--- src/pages/promo.vue
+++ src/pages/promo.vue
@@ -6,10 +6,10 @@
                 <h2 class="promo__title">Акции</h2>
                 <div class="promo__cards promo-cards">
                     <div
-                        v-for="item in 6"
+                        v-for="(item, index) in 6"
                         class="promo-cards__item promo-cards-item"
                         :class="{
-                            'promo-cards-item--big': item === 1,
+                            'promo-cards-item--big': item === 1 || isLastCardBig(index, 6),
                             'promo-cards-item--black': item === 1,
                         }"
                     >
@@ -20,7 +20,7 @@
                                 :src="`assets/images/dev/promo/promo-card${item}.png`"
                             />
                         </div>
-                        <div v-if="item === 1" class="promo-cards-item__sticker">- 50%</div>
+                        <div v-if="item < 3" class="promo-cards-item__sticker">- 50%</div>
                         <div class="promo-cards-item__content promo-cards-item-content">
                             <div class="promo-cards-item-content__title">Прояви себя!</div>
                             <div class="promo-cards-item-content__description">
@@ -32,6 +32,8 @@
             </Wrapper>
         </section>
 
+        <CategorySales />
+
         <ProductsSlider>
             <template #title>Топ 10</template>
         </ProductsSlider>
@@ -41,10 +43,15 @@
 
 <script setup lang="ts">
 import Wrapper from '~/components/Wrapper.vue'
-import { definePageMeta } from '#imports'
+import { computed, definePageMeta, useWindowSize } from '#imports'
 definePageMeta({
     title: 'Акции',
 })
+
+const { width } = useWindowSize()
+const isMobile = computed(() => (width.value ? width.value < 768 : false))
+const isLastCardBig = (index: number, cardsLength: number) =>
+    !(cardsLength % 2) && index === cardsLength - 1 && isMobile.value
 </script>
 
 <style lang="sass" scoped>
@@ -81,6 +88,12 @@ definePageMeta({
     display: grid
     grid-template-columns: 1fr 1fr 1fr
     grid-gap: 30px
+    +until-tablet
+        grid-gap: 20px
+    +media-until(767px)
+        grid-template-columns: 1fr 1fr
+    +while-mob
+        grid-gap: 15px
 
 .promo-cards-item
     position: relative
@@ -89,38 +102,69 @@ definePageMeta({
         grid-row: span 2
         & .promo-cards-item-content
             padding: 40px
+            +until-tablet
+                padding: 30px
+            +while-mob
+                padding: 16px
             &__title
                 font-weight: 700
                 font-size: 40px
+                +until-tablet
+                    font-size: 28px
+                +while-mob
+                    font-size: 20px
             &__description
                 font-weight: 700
                 font-size: 20px
+                +until-tablet
+                    font-size: 18px
+                +while-mob
+                    font-weight: 400
+                    font-size: 16px
+
+        & .promo-cards-item__sticker
+            width: 137px
+            height: 137px
+            font-weight: 700
+            font-size: 40px
+            top: 30px
+            right: 30px
+            +until-tablet
+                top: 16px
+                right: 16px
+                font-size: 24px
+                width: 80px
+                height: 80px
+            +while-mob
+                top: 8px
+                right: 8px
+                font-size: 14px
+                width: 40px
+                height: 40px
+
     &--black
         & .promo-cards-item-content
             color: $color_onsurface_primary
     &__sticker
         z-index: 3
-        width: 137px
-        height: 137px
         border-radius: 50%
         position: absolute
-        top: 30px
-        right: 30px
         background-color: $color_onsurface_quaternary
         display: flex
         align-items: center
         justify-content: center
         color: $color_surface_positive
         transform: rotate(13.6deg)
-        font-weight: 700
-        font-size: 40px
         line-height: 1
+        top: 16px
+        right: 16px
+        font-size: 24px
+        width: 80px
+        height: 80px
         +until-tablet
-            top: 16px
-            right: 16px
-            font-size: 24px
-            width: 80px
-            height: 80px
+            font-size: 20px
+            width: 60px
+            height: 60px
         +while-mob
             top: 8px
             right: 8px
@@ -136,14 +180,30 @@ definePageMeta({
     grid-gap: 16px
     padding: 24px
     color: $color_surface_primary
+    +until-tablet
+        padding: 18px
+        grid-gap: 10px
+    +while-mob
+        padding: 8px
+        grid-gap: 5px
+
     &__title
         font-weight: 700
         font-size: 20px
         line-height: 1.25
+        +until-tablet
+            font-size: 18px
+        +while-mob
+            font-size: 16px
     &__description
         font-weight: 700
         font-size: 16px
         line-height: 1.2
+        +until-tablet
+            font-size: 14px
+        +while-mob
+            font-weight: 400
+            font-size: 12px
 
 .promo-cards-item-bg
     position: relative
